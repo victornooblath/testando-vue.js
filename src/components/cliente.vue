ee4701a39a608811c9df495333284c922bc718cc
Paulo Victor Nooblath Medeiros <Paulov2001@outlook.com>
2021-04-22 16:05:51 -0300

    Update cliente.vue

--- src/components/cliente.vue
+++ src/components/cliente.vue
@@ -1,16 +1,29 @@
 <template>
     <div id="cliente">
-        <h2 id='cliente-nome'>Cliente Aqui - Cliente aqui!</h2>
-        <h3>Descrição do Cliente</h3>
+        
+        <input type="text" v-model="nome">
+        <hr>
+        <input type="text" v-model="descricao">
+        <h2 id='cliente-nome'>{{ nome }}</h2>
+        <p>Descrição do Cliente - {{ descricao }}</p>
+        {{2+2*4}}
     </div>
     
     
 
 </template>
 
-<script>
+<script> 
 export default {
-    
+    data() {
+        return {
+            nome: "vic",
+            numero: "",
+            email: "",
+            idade: 0,
+            descricao: "lorem ipsum",
+        }
+    }
 }
 </script>
 
@@ -20,10 +33,10 @@ export default {
         background-color:rgba(126, 195, 208, 0.885);
         max-width: 600px;
         height: 200px;
+        
     }
 
     #cliente-nome {
         color: rgb(94, 16, 133);
-
     }
 </style>
