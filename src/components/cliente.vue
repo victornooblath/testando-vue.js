a79bd92410bd9a6c06eefb173e61f2d7794ef2ae
Paulo Victor Nooblath Medeiros <Paulov2001@outlook.com>
2021-04-23 18:17:36 -0300

    Update cliente.vue

--- src/components/cliente.vue
+++ src/components/cliente.vue
@@ -3,7 +3,8 @@
         <h4>Nome : {{cliente.nome}} </h4>
         <hr>
         <p>Email : {{cliente.email}}</p>
-        <p>Idade : {{cliente.idade}}</p>
+        <p v-if="showIdade == true">Idade : {{cliente.idade}}</p>
+        <p v-else>O usuário optou por esconder</p>
     </div>
     
     
@@ -18,10 +19,8 @@ export default {
         }
     },
     props: {
-        nome: String,
-        email: String,
-        idade: Number,
-        cliente: Object
+        cliente: Object,
+        showIdade: Boolean
     }
 }
 </script>
