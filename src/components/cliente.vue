4488bd10b509470776dab98cfbd6344df8a0aa56
Paulo Victor Nooblath Medeiros <Paulov2001@outlook.com>
2021-04-24 11:44:46 -0300

    Update cliente.vue

--- src/components/cliente.vue
+++ src/components/cliente.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="cliente">
+    <div :class="{'cliente': false}">
         <h4>Nome : {{cliente.nome}} </h4>
         <hr>
         <p>Email : {{cliente.email}}</p>
@@ -26,13 +26,21 @@ export default {
 </script>
 
 <style scoped>
-    #cliente {
+    .cliente {
         background-color: #ECE5E3;
         max-width: 600px;
         height: 180px;
         padding: 1%;
         margin-top: 2%;
         
+    }
+    .cliente-premium{
+        background-color: #000000;
+        color: yellow;
+        max-width: 600px;
+        height: 180px;
+        padding: 1%;
+        margin-top: 2%;
     }
 
     #cliente-nome {
