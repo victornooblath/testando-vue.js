aaf5ad60b954e4db425d1bd939ccbd89b4b4b2f0
Paulo Victor Nooblath Medeiros <Paulov2001@outlook.com>
2021-04-25 11:33:08 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   
   <div id="app">
-    <cliente :cliente="clienteVictor" :showIdade="true"/>
-    <cliente :cliente="clienteVictor" :showIdade="true"/>
-    <cliente :cliente="clienteVictor" :showIdade="true" />
-    <cliente :cliente="clienteVictor" :showIdade="false"/>
-    <cliente :cliente="clienteVictor" :showIdade="true"/>
     
+
+    <div v-for="(cliente,index) in clientes" :key="cliente.id">
+      <h4>{{ index + 1 }}</h4>
+      <cliente :cliente="cliente"/>
+      <h4>Edição: </h4>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+      <hr>
+
+    </div>
+
   </div>
 
 </template>
@@ -23,7 +29,27 @@ export default {
         nome: "victor Nooblath",
         email: "Victor@gmail.com",
         idade: 89
-      }
+      },
+      clientes: [
+        {
+          id: 22,
+          nome: "Carlos André",
+          email: "Carlos@gmail.com",
+          idade: 49
+        },
+        {
+          id:45,
+          nome: "Pedro vasconselos",
+          email: "pedro@gmail.com",
+          idade: 19
+        },
+        {
+          id: 46,
+          nome: "joão vasconselos",
+          email: "joão@gmail.com",
+          idade: 19
+        }
+      ]
     }
     
   },
